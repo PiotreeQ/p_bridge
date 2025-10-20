c013ae7e838cfd47e81ad4556d21e1ede2fcd661
pScripts <47689001+PiotreeQ@users.noreply.github.com>
2025-10-20 19:44:53 +0200

    Create wasabi_uikit.lua

---
+++ client/notify/wasabi_uikit.lua
@@ -0,0 +1,24 @@
+if (Config.Notify == 'auto' and not checkResource('wasabi_uikit')) or (Config.Notify ~= 'auto' and Config.Notify ~= 'wasabi_uikit') then
+    return
+end
+
+while not Bridge do
+    Citizen.Wait(0)
+end
+
+if Config.Debug then
+    lib.print.info('[Notify] Loaded: wasabi_uikit')
+end
+
+Bridge.Notify = {}
+
+Bridge.Notify.showNotify = function(message, type)
+    if type == 'inform' then
+        type = 'info'
+    end
+    
+    exports.wasabi_uikit:Notification({
+        title = message,
+        type = type,
+    })
+end
