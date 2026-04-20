5e6fa79e67887ffac488c367285cbc49fe2a9f2a
pScripts <47689001+PiotreeQ@users.noreply.github.com>
2026-04-20 14:21:21 +0200

    v1.2.8

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 author 'pScripts [tebex.pscripts.store]'
 lua54 'yes'
 description 'pScripts - Official Bridge'
-version '1.2.7'
+version '1.2.8'
 
 client_scripts {
     'client/main.lua',
